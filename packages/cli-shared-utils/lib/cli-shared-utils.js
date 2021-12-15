8ee0ad11b5a2151c36392ad813f3525407b92b40
longlong <562172151@qq.com>
2021-12-15 14:20:50 +0800

    'init'

---
+++ packages/cli-shared-utils/lib/cli-shared-utils.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: canlong.shen
+ * @Date: 2021-12-15 14:12:51
+ * @LastEditors: your name
+ * @LastEditTime: 2021-12-15 14:19:10
+ * @Description: file content
+ */
+'use strict';
+
+module.exports = cliSharedUtils;
+
+function cliSharedUtils() {
+    // TODO
+    console.log('this.cliSharedUtils')
+    
+}
---
+++ packages/cli/lib/cli.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: canlong.shen
+ * @Date: 2021-12-15 14:12:05
+ * @LastEditors: your name
+ * @LastEditTime: 2021-12-15 14:18:16
+ * @Description: file content
+ */
+'use strict';
+
+module.exports = cli;
+
+function cli() {
+    // TODO
+
+    console.log('this.cli')
+    
+}
